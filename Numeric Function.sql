593f616a88c944cb20ad0e056d8e8ad8d99b1e20
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-09-24 13:03:54 +0530

    Create Numeric Function.sql

---
+++ Numeric Function.sql
@@ -0,0 +1,14 @@
+-- ROUND: Rounds a number to a specified number of decimal places
+SELECT ROUND(123.456, 2) AS rounded_num;
+
+-- FLOOR and CEIL: Returns the largest integer less than or equal to, or the smallest integer greater than or equal to the number
+SELECT FLOOR(123.456) AS floor_num, CEIL(123.456) AS ceil_num;
+
+-- ABS: Returns the absolute value of a number
+SELECT ABS(-123.45) AS abs_value;
+
+-- MOD: Returns the remainder of a division
+SELECT MOD(10, 3) AS remainder;
+
+-- POWER: Returns the value of a number raised to the power of another number
+SELECT POWER(2, 3) AS power_value;

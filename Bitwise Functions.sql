58ef8f441bceaba0ca1dd4cb1310b070f7927798
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-09-28 15:08:50 +0530

    Create Bitwise Functions.sql

---
+++ Bitwise Functions.sql
@@ -0,0 +1,8 @@
+-- BIT_AND: Returns the bitwise AND of all bits in a column
+SELECT BIT_AND(column1) AS bitwise_and FROM table_name;
+
+-- BIT_OR: Returns the bitwise OR of all bits in a column
+SELECT BIT_OR(column1) AS bitwise_or FROM table_name;
+
+-- BIT_XOR: Returns the bitwise XOR of all bits in a column
+SELECT BIT_XOR(column1) AS bitwise_xor FROM table_name;

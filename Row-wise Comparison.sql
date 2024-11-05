220f20bd11e5edd5f2729b63ea241da0ad4fd766
Devang Giri Goswami <167710924+devanggiri@users.noreply.github.com>
2024-11-05 22:10:31 +0530

    Update Row-wise Comparison.sql

--- Row-wise Comparison.sql
+++ Row-wise Comparison.sql
@@ -1,3 +1,7 @@
 SELECT column1, ARRAY_AGG(column2) AS array_values
 FROM table_name
 GROUP BY column1;
+--eg
+SELECT order_id, ARRAY_AGG(product_id) AS products
+FROM order_items
+GROUP BY order_id;
